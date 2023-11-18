b51614a2b8dce9b73b37794de93fa05b0fc516ed
Nicolo Tafta <venditasetam@gmail.com>
2023-11-18 19:19:50 +0100

    chore: add javadoc in Range and RandomArrayGenerator classes

--- src/RandomArrayGenerator.java
+++ src/RandomArrayGenerator.java
@@ -2,14 +2,30 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 
+/**
+ * A utility class for generating random arrays of different types.
+ *
+ * @param <T> the type of elements in the arrays, must implement Comparable
+ */
 public class RandomArrayGenerator<T extends Comparable<T>> {
 
     private final Class<T> type;
 
+    /**
+     * Constructs a RandomArrayGenerator with the specified type.
+     *
+     * @param type the type of elements in the arrays
+     */
     public RandomArrayGenerator(Class<T> type) {
         this.type = type;
     }
 
+    /**
+     * Generates an array of the specified size with random values.
+     *
+     * @param size the size of the array
+     * @return an array of random values
+     */
     public T[] generateArray(int size) {
         T[] array = (T[]) java.lang.reflect.Array.newInstance(type, size);
 
@@ -28,11 +44,26 @@ public class RandomArrayGenerator<T extends Comparable<T>> {
         return array;
     }
 
+    /**
+     * Reverses the order of elements in the given array.
+     *
+     * @param array the array to reverse
+     * @param <T>   the type of elements in the array
+     * @return the reversed array
+     */
     public static <T> T[] reverseArray(T[] array) {
         Collections.reverse(Arrays.asList(array));
         return array;
     }
 
+    /**
+     * Shuffles the first portion of the array based on the specified level.
+     *
+     * @param array the array to shuffle
+     * @param level the shuffling level (percentage of array to shuffle)
+     * @param <T>   the type of elements in the array
+     * @return the shuffled array
+     */
     public static <T> T[] shuffleArray(T[] array, int level) {
         int size = array.length;
         int endIndex = (int) (size * (level / 100.0));
--- src/Range.java
+++ src/Range.java
@@ -1,24 +1,53 @@
 import java.util.Random;
 
+/**
+ * Represents a numeric range with a start and end value (inclusive).
+ */
 public class Range {
+
     private final int start;
     private final int end;
 
+    /**
+     * Constructs a range with the specified start and end values.
+     *
+     * @param start the start value of the range (inclusive)
+     * @param end   the end value of the range (inclusive)
+     * @throws IllegalArgumentException if start is greater than end
+     */
     public Range(int start, int end) {
+        if (start > end) {
+            throw new IllegalArgumentException("Start value must be less than or equal to end value.");
+        }
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * Gets the start value of the range.
+     *
+     * @return the start value
+     */
     public int getStart() {
         return start;
     }
 
+    /**
+     * Gets the end value of the range.
+     *
+     * @return the end value
+     */
     public int getEnd() {
         return end;
     }
 
+    /**
+     * Generates a random number within the range (inclusive).
+     *
+     * @return a random number within the range
+     */
     public int randomNumberInRange() {
         Random random = new Random();
-        return random.nextInt(start, end);
+        return random.nextInt(end - start + 1) + start;
     }
 }
