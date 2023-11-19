bdcc5cbadc0901b5033be0732932d1e03c234ef8
Sofia d'Atri <datris@usi.ch>
2023-11-19 23:10:07 +0100

    Main
    comments

--- src/main/java/assignment1/Main.java
+++ src/main/java/assignment1/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         ResultStorer.printCSVHeader();
         TesterInteger testerInteger = new TesterInteger();
-        testerInteger.setupInteger(); // returns 100 tests for each algorithm, total 300 tests
+        testerInteger.setupInteger(); // returns 200 tests for each algorithm, total 600 tests
         TesterDouble testerDouble = new TesterDouble();
-        testerDouble.setupDouble(); // returns 100 tests for each algorithm, total 300 tests
+        testerDouble.setupDouble(); // returns 200 tests for each algorithm, total 600 tests
         TesterString testerString = new TesterString();
-        testerString.setupString(); // returns 100 tests for each algorithm, total 300 tests
+        testerString.setupString(); // returns 200 tests for each algorithm, total 600 tests
+
+        // returns a total of 1800 tests
     }
 }
