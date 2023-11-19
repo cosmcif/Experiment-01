e7a08cdbbc8c5fe1e09ef8880db3c28e6a02911a
Nicolo Tafta <venditasetam@gmail.com>
2023-11-19 19:21:51 +0100

    feature: created main class

---
+++ src/TesterInteger.java
@@ -0,0 +1,13 @@
+public class TesterInteger<T> {
+    
+    private final Range range;
+    private final Sorter<T> algorithm;
+    private final T[] array;
+    
+    public TesterInteger(Sorter<T> algorithm, T[] array) {
+        this.range = range;
+        this.algorithm = algorithm;
+        this.array = array;
+    }
+}
+    
