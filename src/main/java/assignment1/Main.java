110a447b921f7fd173601fc5872697061c93dd7f
Nicolo Tafta <venditasetam@gmail.com>
2023-11-19 23:29:50 +0100

    added warmup at each step for safety

--- src/main/java/assignment1/Main.java
+++ src/main/java/assignment1/Main.java
@@ -3,13 +3,17 @@ package assignment1;
 public class Main {
     public static void main(String[] args) {
 
-         WarmUp.warmUp();
-//        ResultStorer.printCSVHeader();
-//        TesterInteger testerInteger = new TesterInteger();
-//        testerInteger.setupInteger();
-//        TesterDouble testerDouble = new TesterDouble();
-//        testerDouble.setupDouble();
-//        TesterString testerString = new TesterString();
-//        testerString.setupString();
+        WarmUp.warmUp();
+        ResultStorer.printCSVHeader();
+        TesterInteger testerInteger = new TesterInteger();
+        testerInteger.setupInteger();
+
+        WarmUp.warmUp();
+        TesterDouble testerDouble = new TesterDouble();
+        testerDouble.setupDouble();
+
+        WarmUp.warmUp();
+        TesterString testerString = new TesterString();
+        testerString.setupString();
     }
 }
