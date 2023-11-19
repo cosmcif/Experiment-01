51e9ff25a8be78c11a04d0f6a701d48743da20d8
Nicolo Tafta <venditasetam@gmail.com>
2023-11-19 23:27:11 +0100

    updated the build.gradle

--- src/main/java/assignment1/Main.java
+++ src/main/java/assignment1/Main.java
@@ -2,12 +2,14 @@ package assignment1;
 
 public class Main {
     public static void main(String[] args) {
-        ResultStorer.printCSVHeader();
-        TesterInteger testerInteger = new TesterInteger();
-        testerInteger.setupInteger(); 
-        TesterDouble testerDouble = new TesterDouble();
-        testerDouble.setupDouble(); 
-        TesterString testerString = new TesterString();
-        testerString.setupString(); 
+
+         WarmUp.warmUp();
+//        ResultStorer.printCSVHeader();
+//        TesterInteger testerInteger = new TesterInteger();
+//        testerInteger.setupInteger();
+//        TesterDouble testerDouble = new TesterDouble();
+//        testerDouble.setupDouble();
+//        TesterString testerString = new TesterString();
+//        testerString.setupString();
     }
 }
