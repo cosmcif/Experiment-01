8044527e68536b95b12c7e7c26e9e4a4fb2b6008
Sofia d'Atri <datris@usi.ch>
2023-11-19 22:57:40 +0100

    Main
    Added print header and Double test.

--- src/main/java/assignment1/Main.java
+++ src/main/java/assignment1/Main.java
@@ -2,8 +2,10 @@ package assignment1;
 
 public class Main {
     public static void main(String[] args) {
-        Tester tester = new Tester();
-        tester.setupInteger(); // returns 100 tests for each algorithm, total 300 tests
-
+        ResultStorer.printCSVHeader();
+        TesterInteger testerInteger = new TesterInteger();
+        testerInteger.setupInteger(); // returns 100 tests for each algorithm, total 300 tests
+        TesterDouble testerDouble = new TesterDouble();
+        testerDouble.setupDouble(); // returns 100 tests for each algorithm, total 300 tests
     }
 }
